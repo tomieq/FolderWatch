6e63e499a241fdcdd7844bc82fe62afd6f9ed4d9
tomieq <tomieq@wp.pl>
2025-05-22 08:33:20 +0200

    Improved PixeledBox.enlarged so it does not exceed image size

--- Sources/BoundingBoxConverter.swift
+++ Sources/BoundingBoxConverter.swift
@@ -13,25 +13,44 @@ struct PixeledBox {
     let height: Int32
 }
 
+struct ImageSize {
+    let width: Int32
+    let height: Int32
+    
+    init(width: any BinaryInteger, height: any BinaryInteger) {
+        self.width = Int32(width)
+        self.height = Int32(height)
+    }
+}
+
 extension PixeledBox {
-    func enlarged(scale: Double) -> PixeledBox {
-        let width = Int32(scale * Double(self.width))
-        let height = Int32(scale * Double(self.height))
+    func enlarged(scale: Double, imageSize: ImageSize) -> PixeledBox {
+        var width = Int32(scale * Double(self.width))
+        var height = Int32(scale * Double(self.height))
         let x = max(0, self.x - ((width - self.width) / 2))
         let y = max(0, self.y - ((height - self.height) / 2))
+        
+        // aligh so the boubding box does not exceed the image size
+        let rightBorder = x + width
+        if rightBorder > imageSize.width {
+            width -= rightBorder - imageSize.width
+        }
+        let bottomBorder = y + height
+        if bottomBorder > imageSize.height {
+            height -= bottomBorder - imageSize.height
+        }
         return PixeledBox(x: x, y: y, width: width, height: height)
     }
 }
 
 struct BoundingBoxConverter {
-    let imageWidth: Int
-    let imageHeight: Int
+    let imageSize: ImageSize
     
     func pixels(from boundingBox: CGRect) -> PixeledBox {
-        let width = Int32(boundingBox.size.width * CGFloat(imageWidth))
-        let height = Int32(boundingBox.size.height * CGFloat(imageHeight))
-        let x = Int32(boundingBox.origin.x * CGFloat(imageWidth))
-        let y = Int32((1 - boundingBox.origin.y) * CGFloat(imageHeight)) - height
+        let width = Int32(boundingBox.size.width * CGFloat(imageSize.width))
+        let height = Int32(boundingBox.size.height * CGFloat(imageSize.height))
+        let x = Int32(boundingBox.origin.x * CGFloat(imageSize.width))
+        let y = Int32((1 - boundingBox.origin.y) * CGFloat(imageSize.height)) - height
         return PixeledBox(x: x, y: y, width: width, height: height)
     }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -8,35 +8,70 @@ import Dispatch
 let env = Env()
 _ = try env.load(filename: "local.env")
 
+let yoloRecognizer = try CustomRecognition(modelPath: "/Users/tomaskuc/dev/FolderWatch/model/YOLOv3.mlmodelc")
+
 let resultDir = env.get("RESULTS_DIRECTORY")!
 _ = try PhotoMonitor { url in
-    do {
-        let data = try Data(contentsOf: URL(filePath: url.path))
-        let image = Image(url: url)!
-        let pixelConverter = BoundingBoxConverter(imageWidth: image.size.width, imageHeight: image.size.height)
-
-        let result = HumanRecognition.process(bytes: data.bytes, timeLimit: 10)
-        switch result {
-        case .success(let boundingBoxes):
-            
-            for boundingBox in boundingBoxes {
-                let box = pixelConverter.pixels(from: boundingBox).enlarged(scale: 1.6)
-                print("Found person: x: \(box.x), y: \(box.y), width: \(box.width), height: \(box.height)")
-                
-                try image
-                    .cropped(to: Rectangle(x: box.x, y: box.y, width: box.width, height: box.height))?
-                    .export(as: .jpg(quality: 80))
-                    .write(to: URL(fileURLWithPath: "\(resultDir)/result-\(Int.random(in: 1...Int.max)).jpg"))
-            }
-        case .failure(_):
-            break
-            
-        }
-    }
-    catch {
-        print("Error \(error)")
-    }
+    processImage(url: url)
 }
+        
+
+//        let result = HumanRecognition.process(bytes: data.bytes, timeLimit: 10)
+//        switch result {
+//        case .success(let boundingBoxes):
+//            
+//            for boundingBox in boundingBoxes {
+//                let box = pixelConverter.pixels(from: boundingBox).enlarged(scale: 1.6)
+//                print("Found car: x: \(box.x), y: \(box.y), width: \(box.width), height: \(box.height)")
+//                
+//                try image
+//                    .cropped(to: Rectangle(x: box.x, y: box.y, width: box.width, height: box.height))?
+//                    .export(as: .jpg(quality: 80))
+//                    .write(to: URL(fileURLWithPath: "\(resultDir)/result-\(Int.random(in: 1...Int.max)).jpg"))
+//            }
+//        case .failure(_):
+//            break
+//            
+//        }
+        
+        
+//        let yoloRecognizer = try CustomRecognition(modelPath: "/Users/tomaskuc/dev/FolderWatch/model/YOLOv3.mlmodelc")
+//        let carSideRecognizer = try CustomRecognition(modelPath: "/Users/tomaskuc/dev/FolderWatch/model/CarDetector.mlmodelc")
+//        let objectResult = yoloRecognizer.process(bytes: data.bytes, timeLimit: 10)
+//        switch objectResult {
+//        case .success(let objects):
+//            for object in objects where object.label == "car" {
+//                let box = pixelConverter.pixels(from: object.boundingBox).enlarged(scale: 2.3)
+//                print("Found \(object.label): x: \(box.x), y: \(box.y), width: \(box.width), height: \(box.height)")
+//                
+//                let car = image.cropped(to: Rectangle(x: box.x, y: box.y, width: box.width, height: box.height))!
+//                    
+//                let pixelConverter = BoundingBoxConverter(imageWidth: car.size.width, imageHeight: car.size.height)
+//                let result = carSideRecognizer.process(bytes: try car.export(as: .jpg(quality: 80)).bytes, timeLimit: 10)
+//                switch result {
+//                case .success(let objects):
+//                    for object in objects {
+//                        let box = pixelConverter.pixels(from: object.boundingBox).enlarged(scale: 1.3)
+//                        print("Found \(object.label): x: \(box.x), y: \(box.y), width: \(box.width), height: \(box.height)")
+//                        
+//                        try car
+//                            .cropped(to: Rectangle(x: box.x, y: box.y, width: box.width, height: box.height))?
+//                            .export(as: .jpg(quality: 80))
+//                            .write(to: URL(fileURLWithPath: "\(resultDir)/\(object.label)-\(Int.random(in: 1...Int.max)).jpg"))
+//                    }
+//                case .failure(_):
+//                    break
+//                    
+//                }
+//            }
+//        case .failure:
+//            break
+//        }
+        
+        
+        
+
+
 let processes = ProcessManager().getProcessList().first { $0.name.contains("ffmpeg") }
 print("ffmpeg running: \(processes.notNil)")
 let cameraStream = try CameraStream()
@@ -46,5 +81,32 @@ let cameraStream = try CameraStream()
 //    print("Fire!")
 //    cameraStream.stop()
 //}
+
+
+func processImage(url: URL) {
+    let url = URL(filePath: url.path)
+    do {
+        let data = try Data(contentsOf: url)
+        let image = Image(url: url)!
+        let imageSize = ImageSize(width: image.size.width, height: image.size.height)
+        let pixelConverter = BoundingBoxConverter(imageSize: imageSize)
+        
+        let objects = try yoloRecognizer.process(bytes: data.bytes, timeLimit: 10).get()
+        for (index, object) in objects.enumerated() {
+            let box = pixelConverter.pixels(from: object.boundingBox).enlarged(scale: 1.9, imageSize: imageSize)
+            
+            print("Found \(object.label): x: \(box.x), y: \(box.y), width: \(box.width), height: \(box.height)")
+            
+            try image
+                .cropped(to: Rectangle(x: box.x, y: box.y, width: box.width, height: box.height))?
+                .export(as: .jpg(quality: 80))
+                .write(to: URL(fileURLWithPath: "\(resultDir)/\(object.label)-\(index)-\(url.lastPathComponent).jpg"))
+        }
+        try FileManager.default.removeItem(at: url)
+    }
+    catch {
+        print("Error \(error)")
+    }
+}
 
 RunLoop.main.run()
